44ec2577baa54a92dfc46ff155ee86a559e5dd49
Melissa Pabst <melissapabst@gmail.com>
2018-06-16 00:14:58 -0500

    get the Job with the given ID and pass it into the view

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,10 +1,12 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,6 +26,16 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        Job theJob = jobData.findById(id);
+        model.addAttribute("theJob", theJob);
+
+        //send id to the query string, attributes passed to model
+
+        model.addAttribute("name", theJob.getName());
+        model.addAttribute("employer", theJob.getEmployer());
+        model.addAttribute("location", theJob.getLocation());
+        model.addAttribute("positionType", theJob.getPositionType());
+        model.addAttribute("coreCompetency", theJob.getCoreCompetency());
 
         return "job-detail";
     }
